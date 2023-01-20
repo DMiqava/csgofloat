bbdad8ffb9c4f7d3bc1b5c1aef54149a0b6c9f42
m3taTron <rootUS3rr@pm.me>
2023-01-20 16:56:25 +0400

    ss

--- contentScript.js
+++ contentScript.js
@@ -1,33 +1,36 @@
-console.log("Content Loaded");
-let logo = document.querySelector(".logo");
+(() => {
+  let logo = document.querySelector(".logo");
+  console.log("Content and logo loaded", logo);
 
-setTimeout(() => {
-  let rows = document.querySelectorAll("table tr");
-  console.log("Table rows: ", rows);
+  window.setTimeout(() => {
+    let rows = document.querySelectorAll("table tr");
+    console.log("Table rows: ", rows);
 
-  rows.forEach((row) => {
-    row.addEventListener("click", (event) => {
-      event.target.parentNode.classList.add("selected");
+    rows.forEach((row) => {
+      row.addEventListener("click", (event) => {
+        event.target.parentNode.classList.add("selected");
 
-      const selectedRows = document.querySelectorAll("table tr.selected");
+        const selectedRows = document.querySelectorAll("table tr.selected");
 
-      const selectedRowsArray = Array.from(selectedRows);
+        const selectedRowsArray = Array.from(selectedRows);
 
-      const selectedProfileLinks = selectedRowsArray.map((row) =>
-        row.querySelector(".playerAvatar").getAttribute("href")
-      );
+        const selectedProfileLinks = selectedRowsArray.map((row) =>
+          row.querySelector(".playerAvatar")?.getAttribute("href")
+        );
 
-      console.log("Selected Profiles", selectedProfileLinks);
+        console.log("Selected Profiles", selectedProfileLinks);
 
-      console.log(selectedRowsArray);
+        console.log(selectedRowsArray);
 
-      chrome.storage.local.set({ selectedRows: selectedProfileLinks }, () => {
-        console.log("Selected rows saved to chrome.storage");
+        chrome.storage.local.set({ selectedRows: selectedProfileLinks }, () => {
+          console.log("Selected rows saved to chrome.storage");
+        });
       });
     });
-  });
+  }, 5000);
 
   window.addEventListener("load", () => {
+    let rows = document.querySelectorAll("table tr");
     chrome.storage.local.get("selectedRows", (result) => {
       // If there are selected rows in storage
       if (result.selectedRows) {
@@ -36,8 +39,10 @@ setTimeout(() => {
         console.log("New Rows", rows);
 
         for (let i = 0; i < rows.length; i++) {
-          console.log("Getting Rows", rows[i])
-          let profileLink = rows[i].querySelector(".playerAvatar")?.getAttribute("href") || ""
+          console.log("Getting Rows", rows[i]);
+
+          let profileLink =
+            rows[i].querySelector(".playerAvatar")?.getAttribute("href") || "";
 
           if (result.selectedRows.includes(profileLink)) {
             rows[i].classList.add("selected");
@@ -46,5 +51,4 @@ setTimeout(() => {
       }
     });
   });
-
-}, 8000);
+})();
